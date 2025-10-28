e3080410901f7096704f9f7c46515724706c6fa5
Endra <endra@ndra.com>
2025-10-29 00:54:45 +0700

    Fix: bundle babel runtime correctly for Cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  resolve: {
+    alias: {
+      '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
+    },
+  },
+  optimizeDeps: {
+    include: [
+      '@babel/runtime',
+      '@babel/runtime/helpers/esm/createSuper',
+      '@babel/runtime/helpers/esm/classCallCheck',
+      '@babel/runtime/helpers/esm/possibleConstructorReturn',
+      '@babel/runtime/helpers/esm/getPrototypeOf',
+    ],
+  },
   build: {
     outDir: 'dist',
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
   },
-  base: './', // <--- tambahkan baris ini
-});
+})
